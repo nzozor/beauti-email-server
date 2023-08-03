fbf8b916ffcfb26b1c5079c70142ca6ee3f5787d
Nicolas Zozor <nick.zozor@gmail.com>
2023-08-03 14:33:41 +0100

    update response

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const transporter = nodemailer.createTransport({
 
 // RESTful API route for sending emails
 app.post("/send-email", (req, res) => {
-  const { to, subject, text, number, firstTimeCustomer } = req.body.data;
+  const { to, subject, text, number, firstTimeCustomer } = req.body;
 
   if (!to || !subject || !text) {
     return res.status(400).json({ error: "Invalid request data" });
